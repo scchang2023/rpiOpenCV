d3715baf56d11718f22c742bca7a2f9bb071549c
scchang <sc.chang@gmail.com>
2024-12-10 23:11:59 +0800

    modified

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ cap.set(3, 360)
 cap.set(4, 240)
 while True:
     _, frame = cap.read()
-    cv2.imshow('original', frame)
+    cv2.imshow('camera', frame)
     k = cv2.waitKey(5) & 0xFF
     if k==27:
         break
